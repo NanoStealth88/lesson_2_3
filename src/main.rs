81c03cf59187666e1f57d01034956c8f0a329b7d
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-18 23:56:21 -0600

    Added large number variable with undescores for readability

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,13 @@ fn main() {
 
     println!("The first number is {} and the second number is {}", first_number, second_number);
 
+    //-----------------------------------------
+    //      Readability of large number
+    //-----------------------------------------
+
+    let large_number: i32 = 1_000_000;      //underscore helps in readability of the number and doesn't affect its value, never knew this before
+
     
+
+
 }
