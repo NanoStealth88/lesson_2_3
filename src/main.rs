c791fe97336c5815ccc8106bc6a81b8912ecafc2
NanoStealth88 <chandler.bartholomew@gmail.com>
2022-12-18 23:53:33 -0600

    comitting new cargo.lock file added multiple variables and how to show them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    
+    //----------------------------------------
+    //      Initializing Multiple Variables
+    //----------------------------------------
+
+    let (first_number, second_number) = (250, 380.93);
+
+    println!("The first number is {} and the second number is {}", first_number, second_number);
+
+    
 }
